ea675d4fa1a2d2cf49da943bade4c5a8571b4d93
sonpham1928 <pson4282@gmail.com>
2025-08-09 14:26:04 +0700

    add-validation

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends Controller
 {
     public function index()
     {
-        return response()->json(Category::all());
+        return response()->json(Category::withCount('books')->get());
     }
 
     public function store(StoreCategoryRequest $request)
