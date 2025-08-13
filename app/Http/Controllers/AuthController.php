31016d404bd3a4ae4d248cf269e5bba5c293997c
sonpham1928 <pson4282@gmail.com>
2025-08-14 03:35:28 +0700

    fix-password-confirmation

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -19,7 +19,7 @@ class AuthController extends Controller
         $request->validate([
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|',
+            'password' => 'required|string|min:8|confirmed',
         ]);
         $user = User::create([
             'name' => $request->name,
