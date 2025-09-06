aa3e40b2555afe8c3e195c38f4d0fdf9d04a7572
sonpham1928 <pson4282@gmail.com>
2025-09-06 18:48:50 +0700

    rebuild-order-change-request-controller

--- app/Http/Controllers/OrderChangeRequestController.php
+++ app/Http/Controllers/OrderChangeRequestController.php
@@ -5,20 +5,26 @@ namespace App\Http\Controllers;
 use App\Models\OrderChangeRequest;
 use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\StoreOrderChangeRequest;
+use App\Http\Requests\UpdateOrderChangeRequest;
+use App\Models\Order;
 
 class OrderChangeRequestController extends Controller
 {
     public function index()
     {
-        $data = OrderChangeRequest::all();
+        $data = OrderChangeRequest::with('user')->get();
         return response()->json($data);
     }
 
-    public function store(StoreOrderChangeRequest $request){
+    public function store(StoreOrderChangeRequest $request, $id){
         $request->validated();
 
+        $order = Order::where('id', $id)    
+        ->where('user_id', Auth::id())
+        ->firstOrFail();
+
         $changeRequest = OrderChangeRequest::create([
-            'order_id' => $request->order_id,
+            'order_id' => $order->id,
             'user_id' => Auth::id(),
             'note' => $request->note,
         ]);
@@ -29,4 +35,24 @@ class OrderChangeRequestController extends Controller
         ], 201);
     }
 
+    public function showAdminResponse($id)
+    {
+        $data = OrderChangeRequest::where($id,Auth::id())->select('status','admin_response')->with('order')->get();
+        return response()->json($data);
+    }
+
+    public function updateAdminResponse(UpdateOrderChangeRequest $request, $id)
+    {
+        $request->validated();
+
+        $changeRequest = OrderChangeRequest::where('order_id', $id)->update([
+            'admin_response' => $request->admin_response,
+            'status' => $request->status,
+        ]);
+
+        return response()->json([
+            'message' => 'Cập nhật yêu cầu thành công.',
+            'data' => $changeRequest
+        ], 200);
+    }
 }
--- app/Http/Requests/StoreOrderChangeRequest.php
+++ app/Http/Requests/StoreOrderChangeRequest.php
@@ -3,6 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Order;
 
 class StoreOrderChangeRequest extends FormRequest
 {
